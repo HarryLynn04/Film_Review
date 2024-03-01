13dc3a9c5036823b0d00c02c8c1cbcdd3fc887cf
“Jack <2756698P@student.gla.ac.uk>
2024-03-01 16:03:11 +0000

    more pages added

--- Film_Review/urls.py
+++ Film_Review/urls.py
@@ -5,4 +5,10 @@ app_name = 'ReviewFlix'
 
 urlpatterns = [
     path('', views.home, name='Home'),
+    path('faq/', views.faq, name='FAQ'),
+    path('login/', views.login, name='Login'),
+    path('register/', views.register, name='Register'),
+    path('watchlist/', views.watchlist, name='Watchlist'),
+    path('addAMovie/', views.addAMovie, name='AddAMovie'),
+    
 ]
--- Film_Review/views.py
+++ Film_Review/views.py
@@ -6,3 +6,22 @@ def home(request):
     return render(request, 'ReviewFlix/Home.html', context=context_dict)
 
 # Create your views here.
+def faq(request):
+    context_dict= {}
+    return render(request, 'ReviewFlix/FAQ.html', context=context_dict)
+
+def login(request):
+    context_dict= {}
+    return render(request, 'ReviewFlix/Login.html', context=context_dict)
+
+def register(request):
+    context_dict= {}
+    return render(request, 'ReviewFlix/Register.html', context=context_dict)
+
+def watchlist(request):
+    context_dict= {}
+    return render(request, 'ReviewFlix/Watchlist.html', context=context_dict)
+
+def addAMovie(request):
+    context_dict= {}
+    return render(request, 'ReviewFlix/AddAMovie.html', context=context_dict)   
