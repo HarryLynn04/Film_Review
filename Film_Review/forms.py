c94a80493578b3e2abc140a458916a39ccbe8688
“Khushi <2796649m@student.gla.ac.uk>
2024-03-12 14:34:45 +0000

    user authen final

--- Film_Review/forms.py
+++ Film_Review/forms.py
@@ -7,7 +7,7 @@ class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('username', 'password',)
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
