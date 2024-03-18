6b6345e2eb8773faecf54a3acdfd86651cdb3d2c
Yeva <2833792m@student.gla.ac.uk>
2024-03-18 03:38:27 +0000

    updated homepage

--- static/js/script.js
+++ static/js/script.js
@@ -1,58 +1,22 @@
+let myButtonContainer = document.querySelector(".btn_up"); // Target the container
 
+window.onscroll = function() {scrollFunction()};
 
-function formatText(command) {
-    var textArea = document.getElementById('id_Description');
-    var start = textArea.selectionStart;
-    var end = textArea.selectionEnd;
-    var text = textArea.value;
-    var tag = '';
-
-    switch (command) {
-        case 'bold':
-            tag = 'b';
-            break;
-        case 'italic':
-            tag = 'i';
-            break;
-        case 'underline':
-            tag = 'u';
-            break;
-
-        case 'spoiler':
-            tagOpen = "<span class='spoiler' onclick='toggleSpoiler(this)' style='background-color: black; color: black; cursor: pointer;'>";
-            tagClose = "</span>";
-            break;
-        
-    }
-
-    var beforeText = text.substring(0, start);
-    var selectedText = text.substring(start, end);
-    var afterText = text.substring(end);
-    var tagOpen = '<' + tag + '>';
-    var tagClose = '</' + tag + '>';
-
-    textArea.value = beforeText + tagOpen + selectedText + tagClose + afterText;
-
-    var newPos = start + tagOpen.length + selectedText.length + tagClose.length;
-    textArea.setSelectionRange(newPos, newPos);
-}
-
-function displayFormattedReview() {
-    var textArea = document.getElementById('id_Description');
-    var formattedTextDiv = document.getElementById('formattedText');
-    var htmlContent = textArea.value.replace(/\n/g, '<br>');
-
-    formattedTextDiv.innerHTML = htmlContent;
-}
-
-
-function toggleSpoiler(element) {
-    if (element.style.color === "black") {
-        element.style.color = "white"; 
-    } else {
-        element.style.color = "black"; 
-    }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButtonContainer.style.display = "block"; // Show the container
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    myButtonContainer.style.display = "none"; // Hide the container
+    document.getElementById("myBtn").style.display = "none";
+  }
 }
 
 
 
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
